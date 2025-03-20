2282bb25303f83f6e2ee2ea6a3f4fd1dd12d3189
JHGit-hub <julien.hardy2104@gmail.com>
2025-03-20 15:35:58 +0100

    update - JS(burgers)

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
 // JS pour page burgers
 if(window.location.pathname === "/pages/burgers.html"){
     fetch("../mcdo.json")
-    .then(function(response) {
-        if(!response.ok){
-            throw new Error ("Le fichier Json n'a pas pu être charger.");
-        }
-        return response.json();
-    })
+        .then(function(response) {
+            if(!response.ok){
+                throw new Error ("Le fichier Json n'a pas pu être charger.");
+            }
+            return response.json();
+        })
 
-    .then(function(data) {
-        let burgersListe = document.getElementById('burgersListe');
+        .then(function(data) {
+            let burgersListe = document.getElementById("burgersListe");
 
-    // card burgers
+        // card burgers
 
-        for(let i = 0; i<data.burgers.length; i++ ){
-            let burgers = data.burgers[i];
+            for(let i = 0; i<data.burgers.length; i++ ){
+                let burgers = data.burgers[i];
 
-        // creation des elements
-            let cardBurgers = document.createElement("div");
-            let divImage = document.createElement("div");
-            let divName = document.createElement("div");
-            let divCart = document.createElement("div");
-            let divBtnMore = document.createElement("div");
-            let name = document.createElement("h2");
-            let price = document.createElement("h3");
+            // creation des elements
+                let cardBurgers = document.createElement("div");
+                let divImage = document.createElement("div");
+                let divName = document.createElement("div");
+                let divCart = document.createElement("div");
+                let divBtnMore = document.createElement("div");
+                let name = document.createElement("h2");
+                let price = document.createElement("h3");
 
-        // liens avec le Json
-            divImage.innerHTML = `<img src='${burgers.image}'>`;
-            name.textContent = burgers.name;
-            price.textContent = burgers.price;
-            divName.innerHTML = name;
-            divCart.innerHTML = `<span>Ajouter au panier </span> '${price}'€`;
-            divBtnMore.innerHTML = `<button onclick="viewDetails('${burgers[i].id}')"><img src='assets/icons/eye.png'></button>`;
+            // liens avec le Json
+                divImage.innerHTML = `<img src="../assets/${burgers.image}">`;
+                name.textContent = burgers.name;
+                price.textContent = `<span>Ajouter au panier </span> ${burgers.price}€`;
 
-        // integration des elements
-            cardBurgers.appendChild(divImage);
-            cardBurgers.appendChild(divName);
-            cardBurgers.appendChild(divCart);
-            cardBurgers.appendChild(divBtnMore);
+ 
+                divBtnMore.innerHTML = `<button onclick="viewDetails('${burgers.id}')" title="voir plus"><img src='../assets/icon/eye.png'></button>`;
 
-        // integration dans le HTML
-            burgersListe.appendChild(carBurgers);
-        }
-    })
+
+            // integration des elements
+                divCart.appendChild(price);
+                divCart.id = "divCart";
+
+                divName.appendChild(name);
+                divName.id = 'divName';
+
+                cardBurgers.appendChild(divImage);
+                cardBurgers.id = "cardBurgers";
+
+                cardBurgers.appendChild(divName);
+                cardBurgers.appendChild(divCart);
+                cardBurgers.appendChild(divBtnMore);
+
+            // integration dans le HTML
+                burgersListe.appendChild(cardBurgers);
+            }
+        });
 }
